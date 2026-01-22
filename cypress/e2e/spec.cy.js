407057f43f9c9354fc4e950586c9a0af405be349
Mateus-de-Morais-Barros <mateusdemorais12345@gmail.com>
2026-01-22 14:10:23 -0300

    adiciona cypress ao projeto

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+describe('Todo App', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('deve adicionar uma nova tarefa', () => {
+    cy.get('#title').type('Estudar Cypress')
+    cy.get('#desc').type('Tenho que estudar a documentação do cypress')
+    cy.get(':nth-child(2) > form > .btn').click()
+    
+    cy.get('h4').should('have.text', 'Estudar Cypress')
+    cy.get('.my-2 > p').should('have.text','Tenho que estudar a documentação do cypress')
+  })
+
+
+  it('deve deletar uma nova tarefa', () => {
+    cy.get('#title').type('Estudar Cypress')
+    cy.get('#desc').type('Tenho que estudar a documentação do cypress')
+    cy.get(':nth-child(2) > form > .btn').click()
+
+    cy.get('.my-2 > .btn').click()
+    
+    cy.get('.my-2').should('not.exist')
+  })
+})
